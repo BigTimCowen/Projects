9a1436b1bf38ff605fec8e312c5aa76f2a0fc75b
BigTimCowen <timcowen@gmail.com>
2025-12-16 23:24:48 -0600

    added image capability

--- OKE_OCI_queries.sh
+++ OKE_OCI_queries.sh
@@ -92,3 +92,6 @@ REGION=sa-saopaulo-1
 INSTANCE_OCID=ocid1.instance.oc1.sa-saopaulo-1.antxeljr2bemolacy3wgcy4pj6yqrjzxlnnqavrluq2uwaxq5k2quq3cynta
 CONSOLE_HISTORY_ID=$(oci --region $REGION compute console-history capture --instance-id $INSTANCE_OCID | jq -r '.data.id')
 oci compute console-history get-content --instance-console-history-id $CONSOLE_HISTORY_ID --length 10000000 --file -
+
+#Manually add image capability to an image
+oci compute image-shape-compatibility-entry add --image-id ocid1.image.oc1.ca-montreal-1.aaaaaaaajvjfqw3jhibunlykampikotas36ce3sb --shape-name BM.GPU.B4.8
